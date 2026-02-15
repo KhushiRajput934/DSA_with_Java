6e3bc386d7f5ff3f649e7914585c809911fb631d
Khushi <kspkhushi.143@gmail.com>
2026-02-15 13:37:11 +0530

    Updated

--- Loops.java
+++ Loops.java
@@ -1,8 +1,8 @@
-import java.util.*;
+// import java.util.*;
 
 public class Loops {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         // ------WHILE LOOP------
         // int counter = 0;
         // while(counter < 100) {
