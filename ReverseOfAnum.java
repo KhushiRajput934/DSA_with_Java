32f7bd82e53971ac637042cb3245fc018402e8a2
Khushi <kspkhushi.143@gmail.com>
2026-02-18 12:33:23 +0530

    Num

---
+++ ReverseOfAnum.java
@@ -0,0 +1,11 @@
+public class ReverseOfAnum {
+    public static void main(String[] args) {
+        int n = 342875;
+
+        while (n > 0){
+            int lastDigit = n % 10 ;
+            System.out.print(lastDigit);
+            n = n / 10;
+        }
+    }
+}
